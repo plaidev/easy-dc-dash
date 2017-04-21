754c2f1e70dddef7a1d8636a0409915a7072443c
elcih17 <hicle17@gmail.com>
2017-04-21 17:53:02 +0900

    Fix method of getRow, hardcoding of reduceInit

--- libs/chart/data-table.vue
+++ libs/chart/data-table.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="table-paging" v-if="this.useTablePaging">
       Showing <span>{{this.beginRow}}</span>-<span>{{this.endRow}}</span> of <span>{{this.cfSize}}</span>.
-      <button v-bind="{disabled: isFirstPage}" @click="prevPage()">Prev</button>
-      <button v-bind="{disabled: isLastPage}" @click="nextPage()">Next</button>
+      <button :disabled="isFirstPage" @click="prevPage()">Prev</button>
+      <button :disabled="isLastPage" @click="nextPage()">Next</button>
     </div>
     <table class="krt-dc-data-table table table-hover" :id="id"></table>
   </div>
@@ -17,25 +17,20 @@ import Store from '../store'
 export default {
   extends: Base,
   props: {
-    dimension: {
-      type: String,
-      default: 'd.site_name'
-    },
     chartType: {
       type: String,
       default: 'dataTable'
     },
     columns: {
-      type: Object
+      type: String
     },
     // row order
     sortBy: {
-      type: String,
-      default: 'site_name'
+      type: String
     },
     order: {
       type: String,
-      default: 'd3.descending'
+      default: 'descending'
     },
     // chart style
     width: {
@@ -65,10 +60,20 @@ export default {
       ofs: this.offset,
       pag: this.rowsPerPage,
       cfSize: Store.getCfSize(),
-      columnSettings: []
+      columnSettings: [],
+      _sortBy: this.sortBy
     }
   },
   computed: {
+    getColsExtractor: function() {
+      return new Function('d', 'return ' + this.columns)
+    },
+    cols: function() {
+      return this.getColsExtractor((this.firstRow))
+    },
+    colsKeys: function() {
+      return this.getKeys(this.cols)
+    },
     beginRow: function() {
       return this.ofs
     },
@@ -77,9 +82,6 @@ export default {
     },
     firstRow: function() {
       return this.grouping.top(1)[0]
-    },
-    cols: function() {
-      return Object.keys(this.getRow(this.firstRow))
     },
     isFirstPage: function() {
       return ((this.ofs - this.pag) < 0) ? 'true' : null
@@ -92,35 +94,33 @@ export default {
     extractDimensionName: function(name) {
       return name.replace(/d\./, '')
     },
-    registerDimension: function() {
+    _registerDimension: function() {
       const getter = this.getDimensionExtractor;
       const grouping = getter(this.dimension)
       return Store.registerDimension(this.dimension, grouping)
     },
-    getRow: function(v) {
-      const columns = Object.keys(this.columns)
-      const rows = {}
-      columns.forEach((k) => {
-        if (this.columns[k].count) {
-          Object.assign(rows, {
-              [k]: {
-                count: v[this.columns[k].count],
-                value: v[this.columns[k].value]
-              }
-            }
-          )
+    getKeys: function(v) {
+      return Object.keys(this.getColsExtractor(v))
+    },
+    getSchema: function() {
+      const schema = {}
+      this.colsKeys.forEach((k) => {
+        val = this.cols[k]
+        if(val instanceof String || typeof val === 'string') val = '';
+        else if(val instanceof Number || typeof val === 'number') val = 0;
+        else if(val instanceof Object || typeof val === 'object') {
+          val = {count: 0, value:0, per:0}
         }
-        else if (v[k]) {
-          Object.assign(rows, {[k]: v[k]})
-        }
+        Object.assign(schema, {[k]: val})
       })
-      return rows;
+      return schema
     },
     setColumnSettings: function() {
-      this.cols.forEach((k) => {
-        this.columnSettings.push({label: k, format: (d) => d.value[k]})
+      this.colsKeys.forEach((k) => {
+        this.columnSettings.push({label: k, format: (d) => d.value[k].per || d.value[k]})
       })
     },
+    // paging
     updateTable: function() {
       this.chart.beginSlice(this.ofs);
       this.chart.endSlice(this.ofs + this.pag);
@@ -139,53 +139,52 @@ export default {
   mounted: function() {
     const chart = this.chart;
     const dim = Store.getDimension(this.dimension);
-    const dimensionName = this.extractDimensionName(this.dimension)
-    this.registerDimension()
+    const dimensionName = this.extractDimensionName(this.dimension);
+    this._registerDimension()
     this.setColumnSettings()
+    if(!this.sortBy) this._sortBy = this.colsKeys[0]
 
     chart
       .group((d) => d.value[dimensionName])
       .dimension(dim.group().reduce(
         (p, v) => {
-          const vals = this.getRow(v);
-          this.cols.forEach((k) => {
+          const vals = this.getColsExtractor(v);
+          this.colsKeys.forEach((k) => {
             if (k === dimensionName) {
               p[k] = vals[k]
             }
             else if (vals[k].count) {
-              p[k] = (vals[k].count === 0) ? 0 : vals[k].value / vals[k].count
+              p[k].count += vals[k].count;
+              p[k].value += vals[k].value;
+              p[k].per = p[k].count === 0 ? 0 : p[k].value / p[k].count;
             }
             else p[k] += vals[k]
           })
           return p;
         },
         (p, v) => {
-          const vals = this.getRow(v)
-          this.cols.forEach((k) => {
+          const vals = this.getColsExtractor(v);
+          this.colsKeys.forEach((k) => {
             if (k === dimensionName) {
               p[k] = vals[k]
             }
             else if (vals[k].count) {
-              p[k] = (vals[k].count === 0) ? 0 : vals[k].value / vals[k].count
+              p[k].count -= vals[k].count;
+              p[k].value -= vals[k].value;
+              p[k].per = p[k].count === 0 ? 0 : p[k].value / p[k].count;
             }
             else p[k] -= vals[k]
           })
           return p;
         },
         () => {
-          return {
-            site_name: '',
-            pv: 0,
-            session_cnt: 0,
-            bounce_cnt: 0,
-            bounce_rate: {count: 0,value: 0}
-          }
+          return this.getSchema()
         }
       ))
       .size(Infinity)
       .showGroups(false)
       .columns(this.columnSettings)
-      .sortBy((d) => d[this.sortBy])
+      .sortBy((d) => d[this._sortBy])
       .order(d3[this.order])
     this.updateTable()
     return chart.render();
