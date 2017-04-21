3afda2be1368c2313887b5ddb1b4d69cf8fdcf9f
Naoto Kato <naoto.kato@plaid.co.jp>
2017-04-21 14:48:40 +0900

    add generateExtractor util

--- libs/utils/index.js
+++ libs/utils/index.js
@@ -1,3 +1,4 @@
+
 export function generateUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
@@ -5,6 +6,44 @@ export function generateUUID() {
   })
 }
 
+
 export function generateDomId() {
   return 'id-' + generateUUID();
+}
+
+
+// String(js): <data-table columns="{key: d.key}"></data-table>
+// Object: <data-table :columns="{key: "key"}"></data-table>
+// Array: <data-table :columns="["key"]"></data-table>
+// Function: <data-table :columns="customParser"></data-table>
+export function generateExtractor (rule) {
+  if (typeof rule === 'function' || rule instanceof Function) {
+    return rule
+  }
+
+  else if (typeof rule === "string" || rule instanceof String) {
+    return new Function('d', 'return ' + rule)
+  }
+
+  else if (rule instanceof Array) {
+    return (d) => {
+      row = {}
+      rule.forEach((k) => {
+        row[k] = d[rule[k]]
+      })
+      return row
+    }
+  }
+
+  else if (rule instanceof Object) {
+    return (d) => {
+      row = {}
+      Object.keys(rule).forEach((k) => {
+        row[k] = d[rule[k]]
+      })
+      return row
+    }
+  }
+
+  return // else
 }
