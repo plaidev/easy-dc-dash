7b8d47579876fd7ddd503eeb2c02cefa11627798
elcih17 <hicle17@gmail.com>
2017-04-24 12:27:30 +0900

    Add list-row chart

---
+++ libs/chart/list-row.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="krt-dc-list-row" :id="id">
+    <a class="reset" style="display: none">reset</a>
+  </div>
+</template>
+
+<script lang='js'>
+
+import d3 from "d3"
+import Base from './_base'
+import Store from '../store'
+
+export default {
+  extends: Base,
+
+  props: {
+    chartType: {
+      type: String,
+      default: 'rowChart'
+    },
+    barHeight: {
+      type: Number,
+      default: 30
+    },
+    gap: {
+      type: Number,
+      default: 5
+    },
+    labelOffsetX: {
+      type: Number,
+      default: 10
+    },
+    labeloffsetY: {
+      type: Number,
+      default: 15
+    },
+    titleLabelOffsetX: {
+      type: Number,
+      default: 2
+    },
+    elasticX: {
+      type: Boolean,
+      default: true
+    },
+    renderTitleLabel: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      cfSize: Store.getCfSize()
+    }
+  },
+  mounted: function() {
+    const chart = this.chart;
+    // const count = top(N)
+    chart
+      .width(this.width)
+      .height(this.cfSize * this.barHeight)
+      // .group(dim.group().reduceSum((d) => d.pv))
+      .x(d3.scale.linear().domain([0, this.cfSize]))
+      .labelOffsetX(this.labelOffsetX)
+      .labelOffsetY(this.labeloffsetY)
+      .elasticX(this.elasticX)
+      .renderTitleLabel(this.renderTitleLabel)
+      .titleLabelOffsetX(this.titleLabelOffsetX)
+      // .gap(this.gap)
+      // .fixedBarHeight(this.height - (count + 1) * this.gap - this.barHeight / count)
+      .ordinalColors(['#bd3122', '#3182bd', '#6baed6', '#9ecae1', '#c6dbef', '#dadaeb', '#d66b6e'])
+      .ordering(function(d) { return -d.value })
+    return chart.render();
+  }
+}
+
+</script>
