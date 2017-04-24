64ae79039d5bc110777c4fd3fa63350d8ecd582b
elcih17 <hicle17@gmail.com>
2017-04-24 12:27:30 +0900

    Add list-row chart

--- libs/chart/index.js
+++ libs/chart/index.js
@@ -2,6 +2,7 @@ import Base from './_base'
 import {compose} from './_composite'
 import SegmentPie from './segment-pie.vue'
 import WeekRow from './week-row.vue'
+import ListRow from './list-row.vue'
 import RateLine from './rate-line.vue'
 import StackedLines from './stacked-lines.vue'
 import GeoJP from './geo-jp.vue'
@@ -10,6 +11,7 @@ import DataTable from './data-table.vue'
 const components = {
   'segment-pie': SegmentPie,
   'week-row': WeekRow,
+  'list-row': ListRow,
   'rate-line': RateLine,
   'stacked-lines': StackedLines,
   'geo-jp': GeoJP,
@@ -28,6 +30,7 @@ export default {
   RateLine: RateLine,
   StackedLines: StackedLines,
   WeekRow: WeekRow,
+  ListRow: ListRow,
   SegmentPie: SegmentPie,
   GeoJP: GeoJP,
   DataTable: DataTable,
