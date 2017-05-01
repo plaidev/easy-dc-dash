1288babf1f8d14e277d277957e53a7f2c4bb9ed0
elcih17 <hicle17@gmail.com>
2017-05-01 17:08:36 +0900

    Add reset-all-button

---
+++ libs/chart/reset-all-button.vue
@@ -0,0 +1,31 @@
+<template>
+  <a class="btn btn-danger btn-lg" @click="resetAll">Reset All</a>
+</template>
+
+<script lang="js">
+import dc from 'dc'
+
+
+export default {
+  methods: {
+    resetAll: function() {
+      dc.filterAll();
+      dc.renderAll();
+    }
+  }
+}
+</script>
+
+<style scoped>
+  a.btn {
+    cursor: pointer;
+  }
+  a.btn.btn-danger {
+    color: #fff !important;
+    border-color: #AB312A;
+    font-weight: bold;
+  }
+  a.btn.btn-danger:hover {
+    opacity: .6;
+  }
+</style>
