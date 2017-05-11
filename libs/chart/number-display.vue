1901a69eb6a7bc13c9833166896ac5519e33706f
elcih17 <hicle17@gmail.com>
2017-05-11 12:39:14 +0900

    Add Number Display

---
+++ libs/chart/number-display.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="krt-dc-number-display nd-box" :id="id">
+    <span class="nd-box-label"></span>
+  </div>
+</template>
+
+<script lang='js'>
+
+import Base from './_base'
+import Store from '../store'
+
+function _extractName(s) {
+    return s.replace(/d\./, '')
+}
+
+export default {
+  extends: Base,
+
+  props: {
+    chartType: {
+      type: String,
+      default: 'numberDisplay'
+    },
+    boxLabel: {
+      type: String
+    },
+    numberFormat: {
+      type: String,
+      default: '.2s'
+    }
+  },
+  computed: {
+    _boxLabel: function() {
+      return _extractName(this.dimension) + `[${_extractName(this.reduce)}]`
+    }
+  },
+  mounted: function() {
+    const chart = this.chart;
+    const boxLabel = this.boxLabel || this._boxLabel
+    document.getElementsByClassName('nd-box-label')[0].innerHTML = `${boxLabel}`
+
+    chart
+      .valueAccessor((d) => d.value)
+      .formatNumber(d3.format(this.numberFormat))
+      .html({
+        none:"<span class=\"number-display\">0</span>"
+      })
+    return chart.render();
+  }
+}
+</script>
+
+<style scoped>
+.nd-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+  height: 100px;
+  background: #2AAB9F;
+  border-radius: 5px;
+}
+.nd-box .nd-box-label {
+  color: #FFF;
+  font-size: 12px;
+}
+.nd-box span.number-display {
+  color: #FFF;
+  font-weight: bold;
+  font-size: 48px;
+}
+</style>
