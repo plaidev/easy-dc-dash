94b4ed19b17c019901b9fdbed78363ad02cd0656
Naoto Kato <naoto.kato@plaid.co.jp>
2017-05-15 16:36:52 +0900

    [multidim-pie] add new chart

---
+++ libs/chart/multi-dimension-pie.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="krt-dc-multidim-pie" :id="id">
+    <reset-button v-on:reset="removeFilterAndRedrawChart()"></reset-button>
+  </div>
+</template>
+
+<script lang='js'>
+import dc from 'dc'
+import Base from './_base'
+import Store from '../store'
+
+export default {
+  extends: Base,
+
+  props: {
+    dimension: {
+      type: String,
+    },
+    chartType: {
+      type: String,
+      default: 'pieChart'
+    },
+    height: {
+      type: Number,
+      default: 160
+    },
+    width: {
+      type: Number,
+      default: 200
+    },
+    useLegend: {
+      type: Boolean,
+      default: true
+    },
+    legendGap: {
+      type: Number,
+      default: 5
+    },
+    legendX: {
+      type: Number,
+      default: 0
+    },
+    legendY: {
+      type: Number,
+      default: 0
+    },
+    legendItemHeight: {
+      type: Number,
+      default: 12
+    },
+    legendItemWidth: {
+      type: Number,
+      default: 70
+    },
+    legendHorizontal: {
+      type: Boolean,
+      default: true
+    }
+  },
+
+  computed: {
+    grouping: function() {
+      const getter = this.getDimensionExtractor;
+      const grouping = (d) => {
+        return getter(d).join(',');
+      }
+      return Store.registerDimension(this.dimensionName, grouping, {dataset: this.dataset});
+    }
+  },
+
+  methods: {
+    segmentLabel: function(segmentId) {
+      let label = segmentId;
+      if (this.labels && segmentId in this.labels) {
+        label = this.labels[segmentId]
+      }
+      else {
+        label = Store.getLabel(segmentId)
+      }
+      return label;
+    }
+  },
+
+  mounted: function() {
+    const chart = this.chart;
+    chart
+      .label((d) => this.segmentLabel(d.key))
+    if(this.useLegend) {
+      chart.legend(dc.legend().gap(this.legendGap).x(this.legendX).y(this.legendY).legendWidth(this.width).itemWidth(this.legendItemWidth).itemHeight(this.legendItemHeight).horizontal(this.legendHorizontal))
+    }
+    return chart.render()
+  },
+
+  destroyed: function() {
+    Store.unregisterDimension(this.dimension, {dataset: this.dataset})
+  }
+}
+
+
+</script>
