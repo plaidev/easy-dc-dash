25776d97db25971c3acb710763b12ff5d88d8adb
Naoto Kato <naoto.kato@plaid.co.jp>
2017-05-19 18:24:34 +0900

    [base] fix card component

--- libs/components/card.vue
+++ libs/components/card.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="outer-container" :style="{ width: width+'px', height: height+'px' }">
+  <div class="outer-container" :style="sizeStyle">
     <div class="inner-container">
-      <div class="card-container" :style="{ width: width+'px', height: height+'px' }">
+      <div class="card-container" :style="sizeStyle">
         <h3 v-text="title" class="title"></h3>
         <div class="render-area">
           <slot></slot>
@@ -25,6 +25,14 @@ export default {
       default: 233
     }
   },
+  computed: {
+    sizeStyle: function() {
+      const style = {};
+      if (this.width) style.width = this.width+'px';
+      if (this.height) style.height = this.height+'px';
+      return style
+    }
+  },
   methods: {
     reset: function() {
       this.$emit('reset')
@@ -36,13 +44,15 @@ export default {
 <style scoped>
 .outer-container {
   position: relative;
-  padding: 2px;
 }
 
 .inner-container {
   background-color: white;
-  width: 100%;
-  height: 100%;
+  position: absolute;
+  top: 2px;
+  bottom: 2px;
+  left: 2px;
+  right: 2px;
 }
 
 .card-container {
@@ -70,124 +80,5 @@ export default {
   align-items: center;
   justify-content: center;
 }
-
-
-
-/* h0系列 */
-.fb-lv-h0 {
-  display: flex;
-  flex-direction: row;
-  width: 987px;
-  height: 610px;
-}
-
-.fb-lv-h0 .fb-lv,
-.fb-lv-v1 {
-  display: flex;
-  flex-direction: column;
-  width: 377px;
-  height: 610px;
-}
-
-.fb-lv-h0 .fb-lv .fb-lv,
-.fb-lv-v1 .fb-lv,
-.fb-lv-h2 {
-  display: flex;
-  flex-direction: row;
-  width: 377px;
-  height: 233px;
-}
-
-.fb-lv-h0 .fb-lv .fb-lv .fb-lv,
-.fb-lv-v1 .fb-lv .fb-lv,
-.fb-lv-h2 .fb-lv,
-.fb-lv-v3 {
-  display: flex;
-  flex-direction: column;
-  width: 144px;
-  height: 233px;
-}
-
-.fb-lv-h0 .fb-lv .fb-lv .fb-lv .fb-lv,
-.fb-lv-v1 .fb-lv .fb-lv .fb-lv,
-.fb-lv-h2 .fb-lv .fb-lv,
-.fb-lv-v3 .fb-lv,
-.fb-lv-h4 {
-  display: flex;
-  flex-direction: row;
-  width: 144px;
-  height: 88.9px;
-}
-
-/* h1系列 */
-.fb-lv-h0 .fb-sq .fb-lv,
-.fb-lv-h1 {
-  display: flex;
-  flex-direction: row;
-  width: 610px;
-  height: 377px;
-}
-
-.fb-lv-h1 .fb-lv,
-.fb-lv-v2 {
-  display: flex;
-  flex-direction: column;
-  width: 233px;
-  height: 377px;
-}
-
-.fb-lv-h1 .fb-lv .fb-lv,
-.fb-lv-v2 .fb-lv,
-.fb-lv-h3 {
-  display: flex;
-  flex-direction: row;
-  width: 233px;
-  height: 144px;
-}
-
-.fb-lv-h1 .fb-lv .fb-lv .fb-lv,
-.fb-lv-v2 .fb-lv .fb-lv,
-.fb-lv-h3 .fb-lv,
-.fb-lv-v4 {
-  display: flex;
-  flex-direction: column;
-  width: 88.9px;
-  height: 144px;
-}
-
-
-/* sq系、.fb-sq > .fb-lvは考慮していない */
-.fb-lv-h0 .fb-sq {
-  width: 610px;
-  height: 610px;
-}
-
-.fb-lv-h0 .fb-lv .fb-sq,
-.fb-lv-v1 .fb-sq,
-.fb-lv-h1 .fb-sq {
-  width: 377px;
-  height: 377px;
-}
-
-.fb-lv-h0 .fb-lv .fb-lv .fb-sq,
-.fb-lv-v1 .fb-lv .fb-sq,
-.fb-lv-h2 .fb-sq,
-.fb-lv-h1 .fb-lv .fb-sq,
-.fb-lv-v2 .fb-sq {
-  width: 233px;
-  height: 233px;
-}
-
-.fb-lv-h0 .fb-lv .fb-lv .fb-lv .fb-sq,
-.fb-lv-v1 .fb-lv .fb-lv .fb-sq,
-.fb-lv-h2 .fb-lv .fb-sq,
-.fb-lv-v3 .fb-sq,
-.fb-lv-h1 .fb-lv .fb-lv .fb-sq,
-.fb-lv-v2 .fb-lv .fb-sq,
-.fb-lv-h3 .fb-sq {
-  width: 144px;
-  height: 144px;
-}
-
 
 </style>
