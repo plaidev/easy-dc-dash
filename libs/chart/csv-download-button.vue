5b84e28c6768fc715affc365135c9f28a829376c
elcih17 <hicle17@gmail.com>
2017-07-14 12:14:14 +0900

    apply style to CSV Download Button

--- libs/chart/csv-download-button.vue
+++ libs/chart/csv-download-button.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="download-csv-button">
-    <a class="btn btn-outline-primary btn-lg" @click="downloadCSV('data')">csv download</a>
+    <a class="btn btn-outline-primary" @click="downloadCSV('data')">
+      <i class="fa fa-cloud-download" aria-hidden="true"></i> CSV ダウンロード
+    </a>
   </div>
 </template>
 
@@ -18,13 +20,23 @@ export default {
 </script>
 
 <style scoped>
+.download-csv-button .fa.fa-cloud-download {
+  vertical-align: inherit;
+  padding-right: 4px;
+}
 .download-csv-button a:not([href]):not([tabindex]) {
-  color: #2AAB9F;
+  color: #FFF;
   cursor: pointer;
-  font-weight: bold;
+}
+.download-csv-button .btn {
+  padding: 0.75rem 2rem;
+  font-size: 1.25rem;
+  border-radius: 0.2rem;
+  box-shadow: 0 1px 2px rgba(0,0,0,.25);
 }
 .download-csv-button .btn.btn-outline-primary {
   border-color: #2AAB9F;
+  background: #2AAB9F;
 }
 .download-csv-button .btn.btn-outline-primary:hover {
   color: #2AAB9F;
