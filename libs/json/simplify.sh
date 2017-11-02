0a095c4ca97246765c3e94a62b4e011a692eca41
elcih17 <hicle17@gmail.com>
2017-11-02 15:12:10 +0900

    fix threshold value

--- libs/json/simplify.sh
+++ libs/json/simplify.sh
@@ -5,7 +5,7 @@ for file in $files;
 do
   ext="${file##*.}"
   if [ "$ext" = "topojson" ]; then
-    toposimplify -P 0.001 -o $file.json $file;
+    toposimplify -P 0.01 -o $file.json $file;
   fi
 done
  
