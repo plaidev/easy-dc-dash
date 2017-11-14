f9dfc929b9ea95e69ad8f1279f223ed1089769af
Naoto Kato <naoto.kato@plaid.co.jp>
2017-11-14 18:32:10 +0900

    remove rollup-plugin-css-only

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,6 @@ import vue          from 'rollup-plugin-vue'
 import replace      from 'rollup-plugin-replace'
 import sourcemaps   from 'rollup-plugin-sourcemaps'
 import scss         from 'rollup-plugin-scss'
-import css          from 'rollup-plugin-css-only'
 import json         from 'rollup-plugin-json'
 import nodeBuiltins from 'rollup-plugin-node-builtins'
 import nodeGlobals  from 'rollup-plugin-node-globals'
@@ -58,9 +57,7 @@ export default {
       css: true // dynamically inject
     }),
 
-    scss(),
-
-    css({
+    scss({
       output: 'dist/bundle.css'
     }),
 
