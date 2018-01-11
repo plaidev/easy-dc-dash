1704739cde46942e6b251951bed2af2036a463b3
elcih17 <hicle17@gmail.com>
2018-01-11 16:00:23 +0900

    Add specs (composite charts)

--- test/bar-and-rate.spec.js
+++ test/bar-and-rate.spec.js
@@ -15,18 +15,19 @@ localVue.use(CssModuleTestHelperMixin);
 
 describe('bar-and-rate', () => {
   Store.registerData([
-    { d1: 'a', d2: true, v: 1 },
-    { d1: 'a', d2: false, v: 2 },
-    { d1: 'b', d2: false, v: 3 },
-    { d1: 'b', d2: true, v: 5 }
+    { date: new Date('2017-05-18 10:00'), d1: 'a', d2: true, v1: 1, v2: 10 },
+    { date: new Date('2017-05-19 10:00'), d1: 'a', d2: false, v1: 2, v2: 20 },
+    { date: new Date('2017-05-20 10:00'), d1: 'b', d2: false, v1: 3, v2: 30 },
+    { date: new Date('2017-05-21 10:00'), d1: 'b', d2: true, v1: 5, v2: 50 }
   ]);
 
   const wrapper = mount(localVue.extend(BarAndRate), {
     localVue,
     propsData: {
       id: 'test-bar-and-rate',
-      dimension: 'd1',
-      reduce: 'v'
+      scale: 'time.day',
+      dimension: 'd.date',
+      reduce: '[[d.v1], {count: d.v1, value: d.v2}]'
     },
     attachToDocument: true
   });
